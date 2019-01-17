e5b12f3f864ad220ec51d374c3a0c54fcbd5dace
willthamic <will.hamic@gmail.com>
2019-01-17 17:35:35 -0600

    added chargeset and base of particles

--- ChargeLearning/Pages/Core.cs
+++ ChargeLearning/Pages/Core.cs
@@ -1,6 +1,8 @@
 ﻿using Blazor.Extensions;
 using Microsoft.AspNetCore.Blazor.Components;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChargeLearning.Pages
 {
@@ -149,5 +151,63 @@ namespace ChargeLearning.Pages
             V rUnit = rVector.Unit();
             return magnitude / (rMag * rMag) * rUnit;
         }
+    }
+
+    public class ChargeSet
+    {
+        private HashSet<Charge> set { get; }
+
+        public ChargeSet()
+        {
+            set = new HashSet<Charge>();
+        }
+
+        public void AddRandomCharge(Scene scene)
+        {
+            set.Add(new Charge(scene.random.NextDouble(), V.RandomLocation(scene)));
+        }
+
+        public void RemoveRandomCharge(Scene scene)
+        {
+            if (set.Count == 0)
+                return;
+            int index = scene.random.Next(set.Count);
+            set.Remove(set.ElementAt(index));
+        }
+
+        public void AdjustCount(int idealCount, Scene scene)
+        {
+            while (idealCount > set.Count)
+                AddRandomCharge(scene);
+
+            while (idealCount < set.Count && set.Count != 0)
+                RemoveRandomCharge(scene);
+        }
+
+        public void Mutate(double magnitudeFactor, double locationFactor, double countFactor, Scene scene)
+        {
+            int countAdjuster = (int)((scene.random.NextDouble() - 0.5) * countFactor);
+            AdjustCount(set.Count + countAdjuster, scene);
+            foreach (Charge charge in set)
+            {
+                charge.Mutate(magnitudeFactor, locationFactor, scene);
+            }
+        }
+
+        public V FieldAtPoint(V location)
+        {
+            V field = new V(0, 0);
+            foreach (Charge charge in set)
+            {
+                field += charge.FieldAtPoint(location);
+            }
+            return field;
+        }
+    }
+
+    public class Particle
+    {
+        private V location { get; set; }
+        private V velocity { get; set; }
     }
 }
