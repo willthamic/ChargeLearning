dd8f7e22450da2571807e7c5c2cda97077114efd
willthamic <will.hamic@gmail.com>
2019-01-17 17:31:47 -0600

    added 2d vectors

--- ChargeLearning/Pages/Core.cs
+++ ChargeLearning/Pages/Core.cs
@@ -22,9 +22,62 @@ namespace ChargeLearning.Pages
 
         public void Frame()
         {
-            _ctx.FillStyle = "black";
+            _ctx = _canvas.CreateCanvas2d();
+            Console.WriteLine("Canvas happening");
+            _ctx.FillStyle = "gray";
             _ctx.FillRect(0, 0, 500, 500);
         }
         
+    }
+
+    public class V
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public V(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static V operator +(V a, V b)
+        {
+            return new V(a.x + b.x, a.y + b.y);
+        }
+
+        public static V operator -(V a, V b)
+        {
+            return new V(a.x - b.x, a.y - b.y);
+        }
+
+        public static V operator *(double a, V b)
+        {
+            return new V(a * b.x, a * b.y);
+        }
+
+        public static V RandomLocation(Scene scene)
+        {
+            double x = scene.random.NextDouble() * (scene.xMax - scene.xMin) + scene.xMin;
+            double y = scene.random.NextDouble() * (scene.yMax - scene.yMin) + scene.yMin;
+            return new V(x, y);
+        }
+
+        public static V RandomInRange(Scene scene, double radius)
+        {
+            double mag = scene.random.NextDouble() * radius;
+            double theta = scene.random.NextDouble() * 2 * Math.PI;
+            return mag * new V(Math.Cos(theta), Math.Sin(theta));
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public V Unit()
+        {
+            return (1 / this.Magnitude()) * this;
+        }
     }
 }
